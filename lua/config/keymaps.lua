0e863d620d41f7908eee0a78709b8e6ede6b72ef
Carlos Aviña <juan.avina@wizeline.com>
2023-08-26 16:52:56 -0600

    Add keymaps for inserting empty lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Scroll up/down with cursor in the center
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "<CR>", "m`o<Esc>``")
+vim.keymap.set("n", "<S-CR>", "m`O<Esc>``")
