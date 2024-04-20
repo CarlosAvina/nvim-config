b6c5ca131f5183c720db90243f86534ab7532330
Carlos Aviña <carlosavilu11@gmail.com>
2024-04-20 13:59:20 -0600

    Fix merge conflicts

--- lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
-vim.api.nvim_create_user_command("CopyPath", function()
-  local path = vim.fn.expand("%")
-  vim.fn.setreg("+", path)
-  vim.notify('Copied "' .. path .. '" to the clipboard!')
-end, {})
