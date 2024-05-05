c60eec35c532ff43d61b68c43da5469c5694f6c1
Carlos Aviña <carlosavilu11@gmail.com>
2024-05-04 18:14:12 -0600

    Add formatting and linting configuration

--- init.lua
+++ init.lua
@@ -578,7 +578,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        --[[ tsserver = {}, ]]
         html = {},
         htmx = {},
         --
@@ -611,7 +611,12 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'prettier', -- prettier formatter
+        'stylua', -- lua formatter
+        'isort', -- python formatter
+        'black', -- python formatter
+        'pylint',
+        'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -626,7 +631,18 @@ require('lazy').setup({
             require('lspconfig')[server_name].setup(server)
           end,
         },
-        ensure_installed = { 'tsserver' },
+        ensure_installed = {
+          'tsserver',
+          'html',
+          'cssls',
+          'tailwindcss',
+          'svelte',
+          'lua_ls',
+          'graphql',
+          'emmet_ls',
+          'prismals',
+          'pyright',
+        },
       }
     end,
   },
